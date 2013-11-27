d2c13cefd82815fe444c1eb454c9969dc35e8103
Michael <michael@localhost.localdomain>
2013-11-27 15:18:36 -0500

    wake me up

---
+++ wakemeup.py
@@ -0,0 +1,5 @@
+import time
+from datetime import date
+
+#Wake me up when September ends
+time.sleep((date(date.today().year + 1 if date.today().month > 9 else 0, 10, 1) - date.today()).total_seconds())
